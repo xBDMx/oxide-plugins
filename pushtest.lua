30996874964a64477b9ef65f4f85ebe4f1c59893
Luke Spragg <luke@spragg.me>
2015-01-07 12:18:23 -0500

    Update Push Test to 0.1.1

--- pushtest.lua
+++ pushtest.lua
@@ -1,10 +1,10 @@
 PLUGIN.Title = "Push Test"
-PLUGIN.Version = V(0, 1, 0)
+PLUGIN.Version = V(0, 1, 1)
 PLUGIN.Description = "Push API test plugin."
 PLUGIN.Author = "Wulfspider"
 PLUGIN.HasConfig = false
 
-function PLUGIN:Init()
+function PLUGIN:OnServerInitialized()
     local pushApi = plugins.Find("push")
     pushApi:PushMessage(global.server.hostname, "This is a test of the Push API for Oxide!", "high", "gamelan")
 end
