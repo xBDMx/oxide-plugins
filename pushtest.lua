fbcc5aeabaa23335c1f51feed1e0ed3a7344be65
Luke Spragg <luke@spragg.me>
2015-01-11 12:38:41 -0500

    Update Push Test to 0.1.2

--- pushtest.lua
+++ pushtest.lua
@@ -1,10 +1,11 @@
 PLUGIN.Title = "Push Test"
-PLUGIN.Version = V(0, 1, 1)
+PLUGIN.Version = V(0, 1, 2)
 PLUGIN.Description = "Push API test plugin."
 PLUGIN.Author = "Wulfspider"
 PLUGIN.HasConfig = false
 
 function PLUGIN:OnServerInitialized()
     local pushApi = plugins.Find("push")
+    if not pushApi then print("Push API is not loaded! http://forum.rustoxide.com/plugins/705/"); return end
     pushApi:PushMessage(global.server.hostname, "This is a test of the Push API for Oxide!", "high", "gamelan")
 end
